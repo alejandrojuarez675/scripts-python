d0cc4f3f1dfa4c87f47f3c772960fc8f49585664
Alejandro Juarez <ext_alejjuar@mercadolibre.com>
2023-05-19 15:55:43 -0300

    REFACTOR

--- get-499-timeouts.py
+++ get-499-timeouts.py
@@ -1,23 +1,33 @@
 import pandas as pd
 import re
 
-def cleanMeli(input):
-    return re.sub(r'https:\/\/www\.mercadoli(v|b)re(\.com)\...', "", input)
+# Constants
+HOUR_COLUMN = 0
+HTTP_METHOD_COLUMN = 8
+HTTP_PATH_COLUNM = 12
 
-def cleanSession(input):
-    return re.sub(r'([0-9a-z]){32}\/', "SESSION/", input)
+def __MAIN__():
+    paths = [
+        "./../../../Downloads/f6f10e2f-473b-4f10-8f1f-327689a8595d.csv",
+        "./../../../Downloads/514ca1bd-ad13-4d78-bbfb-b71b5ddc6d5b.csv",
+        "./../../../Downloads/116c9cb1-9f4b-41f0-853a-9053cc907384.csv",
+    ]
 
-def cleanQueryParams(input):
-    return input.split("?")[0]
+    combinedData = {}
+    for path in paths:
+        
+        data = pd.read_csv(
+            filepath_or_buffer = path, 
+            usecols = [HOUR_COLUMN, HTTP_METHOD_COLUMN, HTTP_PATH_COLUNM]
+        ).to_numpy()
 
-def cleanEmptyPages(input):
-    if input == "nan":
-        return "EMPTY-PAGE"
-    else:
-        return input
+        combinedData.update({path: data})
 
-def cleanExternalSites(input):
-    return re.sub(r'http(s):\/\/.*', "EXTERNAL-PAGE", input)
+    sortedResponse = getSortedDictionaryResponse(combinedData)
+
+    for row in sortedResponse:
+        print(str(sortedResponse[row]) + " times -> " + row)
+
     
 def getSortedDictionaryResponse(combinedData):
     response = {}
@@ -47,21 +57,22 @@ def getSortedDictionaryResponse(combinedData):
     return dict(sorted(response.items(), key = lambda x:x[1], reverse=True))
 
 
-def __MAIN__():
-    paths = [
-        "./../../../Downloads/f6f10e2f-473b-4f10-8f1f-327689a8595d.csv",
-        "./../../../Downloads/514ca1bd-ad13-4d78-bbfb-b71b5ddc6d5b.csv",
-        "./../../../Downloads/116c9cb1-9f4b-41f0-853a-9053cc907384.csv",
-    ]
+def cleanMeli(input):
+    return re.sub(r'https:\/\/www\.mercadoli(v|b)re(\.com)\...', "", input)
 
-    combinedData = {}
-    for path in paths:
-        data = pd.read_csv(path, usecols=[0,8,12]).to_numpy()
-        combinedData.update({path: data})
+def cleanSession(input):
+    return re.sub(r'([0-9a-z]){32}\/', "SESSION/", input)
 
-    sortedResponse = getSortedDictionaryResponse(combinedData)
+def cleanQueryParams(input):
+    return input.split("?")[0]
 
-    for row in sortedResponse:
-        print(str(sortedResponse[row]) + " times -> " + row)
+def cleanEmptyPages(input):
+    if input == "nan":
+        return "EMPTY-PAGE"
+    else:
+        return input
+
+def cleanExternalSites(input):
+    return re.sub(r'http(s):\/\/.*', "EXTERNAL-PAGE", input)
 
 __MAIN__()
